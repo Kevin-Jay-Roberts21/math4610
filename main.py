51c8337dd04d4ff4462452b6d0c6c016aa712131
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2022-09-14 20:12:14 -0600

    Added print statements to functions.

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from root_finding_codes.root_finding_functions import *
 # parameters are in order as follows (for the fixed point functions):
 # 1. f function 2. initial root guess 3. tolerance 4. maximum iterations.
 # the third function call is the bisection method and the parameters are
-# in order as follows:
-# 1. f function 2. end point a 3. end point b 4. tolerance.
+# in order as follows: 1. f function 2. end point a 3. end point b 4. tolerance.
+
+# The reason I use x0 in the functions is because x0 is defined to be a value in
+# the functions. If I used a simple x instead, then it would not be recognized because
+# it's not defined. x0 is defined so we use it.
 
 # Task 1 and 2
 fixed_point("x0 - (x0*np.exp(-x0))", 0.0003165, 0.0000001, 10)
 
 # Task 3
-fixed_point("10.14 * np.exp(x0*x0) * cos(np.pi / x0)", 0, 0.0001, 10)
+fixed_point("10.14 * np.exp(x0*x0) * np.cos(np.pi / x0)", -1.9, 0.0001, 10)
 
 # Task 4
+bisection("x0 - (x0*np.exp(-x0))", -5, 5, 0.0001)
 bisection("10.14 * np.exp(x0*x0) * cos(np.pi / x0)", -5, 5, 0.0001)
--- root_finding_codes/root_finding_functions.py
+++ root_finding_codes/root_finding_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# iteration functions. These functions will be using for the fixed point root finding problems
 def g1(f, x):
     gval = x - f
     return gval
@@ -7,32 +8,52 @@ def g2(f, x):
     gval = x + f
     return gval
 
+# fixed point root finding method
 def fixed_point(f, initialx, tol, maxiter):
 
+    # initializing variables: x0, error and number of iterations
     error = 10.0 * tol
     x0 = initialx
     iterations = 0
+
+    # adding print statements for the output to let the user know which iteration method is
+    # being used and the iterations, root location and error.
     print("Results from g1 iteration method:")
+    print("{:<25} {:<25} {:<25}".format('Iterations','Approx. Root Location','Error'))
+
+    # Starting the while loop. If the error is greater than the tolerance and if the iterations
+    # is greater than the maximum number of iterations, then the loop will continue.
     while (error > tol and iterations < maxiter):
-        x1 = g1(float(eval(f)), x0)
-        error = np.abs(x1 - x0)
-        print(x1)
-        x0 = x1
-        iterations += 1
-    print(x1, g1(float(eval(f)), x1))
+        x1 = g1(float(eval(f)), x0) # setting x1 value using g1 iteration function
+        error = np.abs(x1 - x0) # computing the absolute value of the error
+        # printing iteration #, root approx, and error
+        print("{:<25} {:<25} {:<25}".format(iterations, "{:.10f}".format(x1), "{:.10f}".format(error)))
+        x0 = x1 # resetting x0
+        iterations += 1 # increasing iteration
+    print("Final Approximation: " + "{:.10f}".format(x1)) # final approximation
+
+    # adding space for the outputs
+    print()
+    print()
 
     # resetting initial variables to test convergence for g2
+    # the only difference from the code above is that in the following code we'll be using g2
     error = 10.0 * tol
     x0 = initialx
     iterations = 0
     print("Results from g2 iteration method:")
+    print("{:<25} {:<25} {:<25}".format('Iterations', 'Approx. Root Location', 'Error'))
     while (error > tol and iterations < maxiter):
         x1 = g2(float(eval(f)), x0)
         error = np.abs(x1 - x0)
-        print(x1)
+        print("{:<25} {:<25} {:<25}".format(iterations, "{:.10f}".format(x1), "{:.10f}".format(error)))
         x0 = x1
         iterations += 1
-    print(x1, g2(float(eval(f)), x1))
+    print("Final Approximation: " + "{:.10f}".format(x1))
+
+    # adding space for the outputs
+    print()
+    print()
 
 def bisection(f, endpointA, endpointB, tol):
     pass
